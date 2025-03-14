887075fd0b7c7093ccf8e74c5f6ec9a0ed7e18fc
LastQubitDev <lastqubit.dev@gmail.com>
2025-03-15 00:03:17 +0100

    Adding scriptable objects for crafted items and resources

---
+++ Assets/ScriptableObjects/Gameplay/ItemKey.cs
@@ -0,0 +1,21 @@
+namespace ScriptableObjects.Gameplay
+{
+	public enum ItemKey
+	{
+		DragonScale = 0,
+		EmberDust = 1,
+		FireShard = 2,
+		GoldOre = 3,
+		IronOre = 4,
+		DraconicCrown = 5,
+		DraconicShield = 6,
+		EmberBlade = 7,
+		EnchancedEmberBlade = 8,
+		EnchancedRadiantStaff = 9,
+		GoldIngot = 10,
+		FlameGoldIngot = 11,
+		IronIngot = 12,
+		RadiantStaff = 13,
+		SteelIngot = 14,
+	}
+}
